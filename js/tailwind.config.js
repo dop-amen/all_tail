d2e15f2e3ec778ff639df0b4b094d8d49b9c750d
Aminul Islam <aminulabir2004@gmail.com>
2024-09-30 21:59:13 +0600

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+         'navcolor': 'rgba (72, 50,133, 0)',
+         'hovnav': 'rgba(255, 255, 255, 0.5)',
+         'btncolor': '0deg, #c7ff3f 0%, #a3ff54 100%',
+        },
+        maxWidth: {
+            'container': '1320px',
+          },
+          fontFamily:{
+            'goth':'Urbanist',
+            'multi':'Marcellus',
+          },
+          
+          
+      }
+    }
+  }
